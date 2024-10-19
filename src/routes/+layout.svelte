1c38115edb2ec5f6d4cfe4e27f873cf67b2079fb
Lucas Fell <lucas.fell@partner.bmw.com.br>
2024-10-19 16:40:20 -0300

    format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script>
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
-	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from "@inlang/paraglide-sveltekit";
+	import { i18n } from "$lib/i18n";
 
-	import '../app.css';
+	import "../app.css";
 </script>
 
 <ParaglideJS {i18n}>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	import Button from '$lib/components/ui/button/button.svelte';
-	import * as m from '$lib/paraglide/messages.js';
+	import Button from "$lib/components/ui/button/button.svelte";
+	import * as m from "$lib/paraglide/messages.js";
 </script>
 
 <Button>{m.click_me()}</Button>
